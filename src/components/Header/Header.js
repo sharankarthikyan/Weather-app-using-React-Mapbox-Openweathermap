065f8a068264be0c80b3cb58cb4dcfbddfffbd77
Sharan <karthikeyansharan00@gmail.com>
2020-11-20 18:32:28 +0530

    update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Navbar } from "react-bootstrap";
+import Brand from "../../assets/images/TwinkleWeathers2.svg";
 
 const header = () => {
   return (
-    <Navbar bg="dark" variant="dark">
+    <Navbar bg="light" variant="light">
       <div className="container">
-        <Navbar.Brand href="/">Twinkle Weathers</Navbar.Brand>
+        <Navbar.Brand href="/">
+          <img src={Brand} alt="" width="150px" />
+        </Navbar.Brand>
       </div>
     </Navbar>
   );
